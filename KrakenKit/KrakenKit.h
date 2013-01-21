9de11b1ea09dd78a9fe1aeaf45cc217103b0a24a
CodaFi <devteam.codafi@gmail.com>
2013-01-21 11:12:38 -0700

    Removed the old KrakenKit

--- KrakenKit/KrakenKit.h
+++
@@ -1,9 +0,0 @@
-//
-//  KrakenKit.h
-//  KrakenKit
-//
-//  Created by Robert Widmann on 1/10/13.
-//  Copyright (c) 2013 CodaFi. All rights reserved.
-//
-
-#import "KrakenKit/UAGithubEngine/UAGithubEngine/UAGithubEngine.h"
